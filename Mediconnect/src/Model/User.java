975365f07c4265c1525aa0470399047d8181db6f
Venkatprithvigade <gade.venka@northeastern.edu>
2024-12-08 20:50:27 -0500

    User files have been commited

---
+++ Mediconnect/src/Model/User.java
@@ -0,0 +1,59 @@
+
+package Model;
+
+
+public class User {
+    String username, password, name, role, org, orgName;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getOrg() {
+        return org;
+    }
+
+    public void setOrg(String org) {
+        this.org = org;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+         @Override
+    public String toString(){
+        return name;
+    }
+}
