0f624af557f97d09f2e5408a8ea8c45019bbbc91
MansiGondil <gondil.m@northeastern.edu>
2024-12-08 20:36:51 -0500

    Added pharmacysupply class files

---
+++ Mediconnect/src/Model/PharmacySupply.java
@@ -0,0 +1,44 @@
+
+package Model;
+
+
+public class PharmacySupply {
+    String pharmacyId, item, quantity, price;
+
+    public String getPharmacyId() {
+        return pharmacyId;
+    }
+
+    public void setPharmacyId(String pharmacyId) {
+        this.pharmacyId = pharmacyId;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    @Override
+    public String toString(){
+        return item;
+    }
+}
